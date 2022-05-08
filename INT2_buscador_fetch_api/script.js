98ea7760d6bcd51281dedbd405050ffad7e482df
Javier Martínez <jmartinez@develsystems.com>
2022-05-07 20:14:53 -0600

    Filtro añadido

--- INT2_buscador_fetch_api/script.js
+++ INT2_buscador_fetch_api/script.js
@@ -1,29 +1,33 @@
+function obtenerDatos() {
 
-const inputBuscar = document.getElementById('buscar')
+  const inputBuscar = document.getElementById('buscar').value;
 
-function obtenerDatos(){
+  const mostrarData = (data) => {
 
-fetch('https://jsonplaceholder.typicode.com/users')
-
-  .then(response => response.json())
-
-  .then(data => mostrarData(data))
-
-  .catch(error => console.log(error))
-
-
-const mostrarData = (data) => {
     console.log(data)
     let body = ''
-    for (let i=0; i<data.length; i++){
-        body += `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].username}</td><td>${data[i].email}</td><td>${data[i].address.city}</td></tr>`
+    for (let i = 0; i < data.length; i++) {
+      body += `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].username}</td><td>${data[i].email}</td><td>${data[i].address.city}</td></tr>`
     }
 
     document.getElementById('data').innerHTML = body
+  }
+
+
+  if (inputBuscar.length == 0) {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+
+      .then(response => response.json())
+
+      .then(data => mostrarData(data))
+
+      .catch(error => console.log(error))
+
+  } else {
+
+    fetch(`https://jsonplaceholder.typicode.com/users?username=${inputBuscar}`)
+      .then(response => response.json())
+      .then(data => mostrarData(data))
+  }
 }
-
-}
-
-
-
-
